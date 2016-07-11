f27929ba46075a8ec7dd3566ca3b6c7ab064f62a
tuyu5200 <819092969@qq.com>
2016-07-11 11:09:05 +0800

    add user interface

--- src/com/tuyu/service/IUserService.java
+++ src/com/tuyu/service/IUserService.java
@@ -1,7 +1,22 @@
 package com.tuyu.service;
 
+import com.tuyu.model.UserEntity;
+
+import java.util.List;
+
 /**
  * Created by Tuyu on 2016/7/10 18:32 .
  */
 public interface IUserService {
+    //add user
+    boolean addUser(UserEntity userEntity);
+
+    //查询用户
+    List<UserEntity> queryUser(String acount);
+
+    //删除用户
+    boolean deleteUser(String account);
+
+    //修改用户
+    boolean updateUser(UserEntity userEntity);
 }
